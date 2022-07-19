b9279e1a7b61e924fd4cec9022426d88ff358927
Volodymyr <lesksv2000@gmail.com>
2022-07-19 16:47:46 +0300

    add readme

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,11 +1,1 @@
 // <reference types="react-scripts" />
-export type State = {
-  loadData: LoadData;
-};
-
-interface LoadData {
-  date: string
-  base: string
-  symbols: string
-  amount: number
-}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,3 @@
-// import { configureStore, createReducer } from '@reduxjs/toolkit';
-// // eslint-disable-next-line import/extensions,import/no-unresolved
-// import { State } from '../react-app-env';
-// import { setLoadData } from './actions';
-//
-// export type AppDispatch = typeof store.dispatch;
-// export const initialState: State = {
-//   loadData: {
-//     date: '',
-//     base: '',
-//     symbols: '',
-//     amount: 0,
-//   },
-// };
-// const reducer = createReducer(initialState, (builder) => {
-//   builder
-//     .addCase(setLoadData, (state, action) => {
-//       // eslint-disable-next-line no-param-reassign
-//       state.loadData = action.payload;
-//     });
-// });
-//
-// export const store = configureStore({ reducer });
 import { configureStore } from '@reduxjs/toolkit';
 import CurrencyReducer from './CurrencySlice';
 
